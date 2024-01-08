e8cb13398dac43f6f09e95656e51e3562bdab15e
papnineone <134521718+nanibrah@users.noreply.github.com>
2024-01-08 17:36:48 +0700

    Reset

--- EngineCheatFF/A. Download Here/Main.cs
+++ EngineCheatFF/A. Download Here/Main.cs
@@ -1,11 +1,30 @@
-﻿using ColorText;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace mainProgram
 {
     class Program
     {
+        public static void WriteColor(string message, ConsoleColor color)
+        {
+            var pieces = Regex.Split(message, @"(\[[^\]]*\])");
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                string piece = pieces[i];
+
+                if (piece.StartsWith("[") && piece.EndsWith("]"))
+                {
+                    Console.ForegroundColor = color;
+                    piece = piece.Substring(1, piece.Length - 1);
+                }
+
+                Console.Write(piece);
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
         private static void Scam()
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -21,32 +40,57 @@ namespace mainProgram
             Console.Write("Source code: https://github.com/nanibrah/C-sharp\nWriter: Pham Loi ( nanibrah )");
             Console.ReadKey();
         }
+        public static void logTime()
+        {
+            string logFilePath = @"log.txt";
+            string CurrentTime = DateTime.Now.ToString("hh:mm:ss tt");
+            string CurrentDay = DateTime.Now.ToString("dddd:MMMM:yyyy tt");
+            string prettier = "-----------------------------------------------------";
+            string fulltextopentime = $"{prettier}\n| Open at [{CurrentTime}] on [{CurrentDay}] |\n{prettier}";
+            string openTime = $"\n| Open at [{CurrentTime}] on [{CurrentDay}] |\n{prettier}";
+
+            if (File.Exists(logFilePath))
+            {
+                if (File.ReadLines(logFilePath).Count() >= 3)
+                {
+                    File.AppendAllText(logFilePath, openTime);
+                }
+                else
+                {
+                    File.AppendAllText(logFilePath, fulltextopentime);
+                }
+            }
+            else
+            {
+                File.WriteAllText(logFilePath, fulltextopentime);
+            }
+        }
         static void Main(string[] args)
         {
+            logTime();
             string CurrentTime = DateTime.Now.ToString("hh:mm:ss tt");
             int CurrentHeight = Console.WindowHeight;
             int CurrentWidth = Console.WindowWidth;
             Console.SetWindowSize(102, 25);
             if (Console.WindowWidth != 102 || Console.WindowHeight != 25)
             {
-                string logTxtFile = $"Error different window size\nHeight: {CurrentHeight} / 25 - Width: {CurrentWidth} / 102\n{CurrentTime}\n----------------------------------\n";
-                string logTxtFilePath = "log.txt";
-                WriteColorText.WriteColor("[[Error different window size, read log file to know more]", ConsoleColor.Red);
-                if (File.Exists(logTxtFilePath))
+                string ErrorlogTxtFile = $"Error different window size\nHeight: {CurrentHeight} / 25 - Width: {CurrentWidth} / 102\n{CurrentTime}\n----------------------------------\n";
+                string ErrorlogTxtFilePath = "Errorlog.txt";
+                WriteColor("[[Error different window size, read Errorlog file to know more]", ConsoleColor.Red);
+                if (File.Exists(ErrorlogTxtFilePath))
                 {
-                    File.AppendAllText(logTxtFilePath, logTxtFile);
+                    File.AppendAllText(ErrorlogTxtFilePath, ErrorlogTxtFile);
                 } 
                 else
                 {
-                    File.WriteAllText("log.txt", logTxtFile);
+                    File.WriteAllText(ErrorlogTxtFilePath, ErrorlogTxtFile);
                 }
                 Environment.Exit(0);
             }
             Console.WriteLine("         ______             _               _____ _                _     ______ ______ \r\n        |  ____|           (_)             / ____| |              | |   |  ____|  ____|\r\n        | |__   _ __   __ _ _ _ __   ___  | |    | |__   ___  __ _| |_  | |__  | |__   \r\n        |  __| | '_ \\ / _` | | '_ \\ / _ \\ | |    | '_ \\ / _ \\/ _` | __| |  __| |  __|  \r\n        | |____| | | | (_| | | | | |  __/ | |____| | | |  __/ (_| | |_  | |    | |     \r\n        |______|_| |_|\\__, |_|_| |_|\\___|  \\_____|_| |_|\\___|\\__,_|\\__| |_|    |_|     \r\n                       __/ |                                                           \r\n                      |___/");
 
-            WriteColorText.WriteColor($"[[{CurrentTime}] Developer: Pham Loi \n[[{CurrentTime}] Version: 0.0.0\n[[{CurrentTime}] Support emulator: BlueStack 5\n\n", ConsoleColor.Cyan);
+            WriteColor($"[[{CurrentTime}] Developer: Pham Loi \n[[{CurrentTime}] Version: 0.0.0\n[[{CurrentTime}] Support emulator: BlueStack 5\n\n", ConsoleColor.Cyan);
             Console.WriteLine("\t\t[ 1 ] Aim Head  |  [ 2 ] Fix lag   |  [ 3 ] Crosshair |  [ 4 ] Esp line\n\n\t\t\t");
-            //                           ----------|--      ----------|--      ----------|--      ----------|--
             Console.Write("Choose to start program \n>>> ");
 		    switch (Convert.ToInt32(Console.ReadLine()))
 		    {
